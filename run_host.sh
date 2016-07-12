c4e3dacd3fa96f300e28e9307a4ff2dd5945ad41
Wissen XUE <xuezhijian@gmail.com>
2016-07-12 10:41:48 +0800

    add the  -e wqGIT_REPO=url enviroment example

--- run_host.sh
+++ run_host.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker run --name nginx-php-fpm -d -v /your/path/to/www_root/:/var/www/html -p 4080:80 -p 4443:443 wisicn/nginx-php-fpm
+#use environment variable to let docker to git clone Pagekit Repository:
+#docker run --name nginx-php-fpm -d -v /your/path/to/www_root/:/var/www/htmli -e 'GIT_REPO=https://github.com/pagekit/pagekit.git -p 4080:80 -p 4443:443 wisicn/nginx-php-fpm
